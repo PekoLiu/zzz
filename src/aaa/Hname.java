41f5ad7651bf081bfd1eead8b073762c6096a237
pekoliu <479429152@qq.com>
2019-05-29 17:05:21 +0800

    update 01

--- src/aaa/Hname.java
+++ src/aaa/Hname.java
@@ -3,8 +3,8 @@ package aaa;
 public class Hname {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("nihao");
+		System.out.println("haha");
 	}
 
 }
